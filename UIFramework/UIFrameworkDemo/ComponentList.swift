7ff98987d8208a7b3b1fa5522b216a2e16d62aca
TheusMartins <ml_martins@yahoo.com>
2021-03-28 17:59:01 -0300

    Add componentList View on demo app

--- UIFramework/UIFrameworkDemo/ComponentList.swift
+++ UIFramework/UIFrameworkDemo/ComponentList.swift
@@ -7,8 +7,47 @@
 
 import UIKit
 
-class ComponentList: UIViewController {
-    
-
+enum Components {
+    case progressBar
 }
 
+final class ComponentList: UIViewController {
+    // MARK: - Private properties
+    private lazy var tableView: UITableView = {
+        let table = UITableView(frame: .zero, style: .plain)
+        table.delegate = self
+        table.dataSource = self
+        return table
+    }()
+    
+    private let dataSource: [Components] = [.progressBar]
+    
+    // MARK: - Life cycle
+    override func loadView() {
+        super.loadView()
+        view = tableView
+    }
+}
+
+extension ComponentList: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch dataSource[indexPath.row] {
+        case .progressBar: "
+        }
+    }
+}
+
+extension ComponentList: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =  UITableViewCell.init(style: .default, reuseIdentifier: "DefaultCell")
+        cell.selectionStyle = .none
+        switch dataSource[indexPath.row] {
+        case .progressBar : cell.textLabel?.text = "ProgressBar"
+        }
+        return cell
+    }
+}
